fbf1b96b0a91bc345f92a2bb49bc4ad2a8d760df
Michal Mojzík <michal@mojzik.net>
2016-10-18 16:18:27 +0200

    Adding three dots when there are more units than the reported is limited to.

--- src/UnitReporter.cpp
+++ src/UnitReporter.cpp
@@ -8,18 +8,20 @@
 using namespace BT;
 
 EvaluationResult UnitReporter::execute(const std::vector<springai::Unit*> units) {
-  std::string messageComposer("Current units: ");
+  std::string message("Current units: ");
 
   int limit = maxUnitReported_;
   for (auto it = units.begin(); it != units.end() && --limit >= 0; ++it)
   {
     auto unit(*it);
     if (limit != 4)
-      messageComposer += ", ";
-    messageComposer += std::to_string(unit->GetUnitId());
+      message += ", ";
+    message += std::to_string(unit->GetUnitId());
   }
+  if (limit < 0)
+    message += "...";
 
-  callback->GetGame()->SendTextMessage(messageComposer.c_str(), 0);
+  callback->GetGame()->SendTextMessage(message.c_str(), 0);
 
   return btSuccess;
 }
